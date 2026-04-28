f30a7edcbc8ebca4d943795dd920d80219986fd0
Your Name <you@example.com>
2026-04-28 04:45:25 +0000

    Add fallback CONVEX_URL to fix 500 error

--- src/router.tsx
+++ src/router.tsx
@@ -6,10 +6,8 @@ import { ConvexProvider } from 'convex/react'
 import { routeTree } from './routeTree.gen'
 
 export function getRouter() {
-  const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL!
-  if (!CONVEX_URL) {
-    console.error('missing envar CONVEX_URL')
-  }
+  const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL || 'https://anonymous-agent-3210.convex.cloud'
+  
   const convexQueryClient = new ConvexQueryClient(CONVEX_URL)
 
   const queryClient: QueryClient = new QueryClient({
